6843aab5eb3c514b2bbc9418ee9287b3328b18b9
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-10-18 18:48:04 +0200

    qgisimporter - main.cpp to dump the generated mapfile

--- qgisimporter/main.cpp
+++ qgisimporter/main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QtCore/QDebug>
+#include <QTemporaryFile>
+#include <QTextStream>
 
 #include "qgisimporter.h"
 
@@ -10,18 +12,44 @@ int main(int argc, char ** argv) {
 
    QApplication(argc, argv);
 
+
+   MapfileParser * p = NULL;
    if (argc > 1) {
      imp = new QGisImporter(argv[1]);
-     MapfileParser * p = imp->importMapFile();
    } else {
      qDebug() << "Usage: " << argv[0] <<  "<QGis project>";
+     return 0;
    }
 
+   p = imp->importMapFile();
 
-   qDebug() << "QGis importer test about to end";
+   // Touch'ing a temporary file
+   //
+   // Doing so, because the Mapserver API to save a mapfile into a char * is
+   // fiddling with stdout, I don't feel it very secure in term of race
+   // condition, if another thread ever pollutes the standard output during
+   // dumping the memory object.
+   //
+   // See mapfile.c: char* msWriteMapToString(mapObj *map)
+   //
+   QTemporaryFile * f = new QTemporaryFile();
+   f->open();
+   f->close();
+   qDebug() << "";
 
+   p->saveMapfile(f->fileName());
+
+   // reopens the file
+   f->open();
+   QTextStream in(f);
+   qDebug() << in.readAll().toStdString().c_str();
+   f->close();
+
+   if (f)
+     delete f;
    if (imp)
      delete imp;
 
+  qDebug() << "QGis importer test about to end";
   return 0;
 }
