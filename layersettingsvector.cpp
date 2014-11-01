336d3e989e57c24ed321187267f42621251ccd62
Yves Jacolin <yjacolin@free.fr>
2014-11-01 11:04:40 +0100

    forget layersettingsvector.cpp file

--- layersettingsvector.cpp
+++ layersettingsvector.cpp
@@ -1,22 +1,23 @@
 #include "layersettingsvector.h"
 #include "ui_layersettingsvector.h"
 
-
-LayerSettingsVector::LayerSettingsVector(QWidget * parent, MapfileParser * map):
-  QTabWidget(parent), ui(new Ui::LayerSettingsVector), mapfile(map) {
-
+LayerSettingsVector::LayerSettingsVector(QWidget * parent, MapfileParser * mf) :
+  QTabWidget(parent), ui(new Ui::LayerSettingsVector), mapfile(mf)
+{
     ui->setupUi(this);
+    this->settingsUndoStack = new QUndoStack(this);
+
 }
 
+
+//SLOTS
+void LayerSettingsVector::accept() {
+    //this->saveLayerSettings();
+
+    //QDialog::accept();
+}
+/** End SLOTS **/
 
 LayerSettingsVector::~LayerSettingsVector() {
   delete ui;
 }
-
-
-void LayerSettingsVector::accept() {
-  // ...
-}
-
-
-
